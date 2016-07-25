e35e83b21a3fe3549153350b15be227588d3d332
vwxyutarooo <yutaro@studio-rubbish.com>
2016-07-25 15:47:07 +0900

    Add php7.0-mbstring

--- bootstrap.sh
+++ bootstrap.sh
@@ -40,7 +40,7 @@ apt-get install -y memcached build-essential
 apt-get install -y nginx
 
 # Install PHP
-apt-get install php7.0 php7.0-fpm php7.0-mysql php7.0-xml php7.0-curl -y
+apt-get install php7.0 php7.0-fpm php7.0-mysql php7.0-xml php7.0-curl php7.0-mbstring -y
 ## Composer
 php -r 'copy("https://getcomposer.org/installer", "composer-setup.php");'
 php -r 'if (hash_file("SHA384", "composer-setup.php") === "e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae") { echo "Installer verified"; } else { echo "Installer corrupt"; unlink("composer-setup.php"); } echo PHP_EOL;'
